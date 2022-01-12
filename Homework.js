aaee4230ebc0877c21b4f9393c13a2a98bb85f78
MaxFeldman <Mr.Feldman85@gmail.com>
2022-01-12 20:49:57 +0200

    Homework#10 final

--- Homework.js
+++ Homework.js
@@ -1,14 +1,14 @@
 // 1.Just used only "a" and "s" i need print "ananas"
 console.log("a" + ("as" - 2) + "as");
 // 2.function calculate that can perform any arithmetic operations on two numbers (+,-,*,/);
-/*function calculate (num1,num2,action){
+function calculate (num1,num2,action){
     switch(action){
-        case '+': num1 + num2;break;
-        case '-': num1 - num2;break;
-        case '*': num1 * num2;break;
-        case '/': num1 / num2;break;
+        case '+': return num1 + num2;break;
+        case '-': return num1 - num2;break;
+        case '*': return num1 * num2;break;
+        case '/': return num1 / num2;break;
         default:
-            return('wrong action');
+            return("wrong action");
     }
     }
     const cal_1 = calculate(2,3,'+');
@@ -18,14 +18,7 @@ console.log("a" + ("as" - 2) + "as");
     console.log(cal_1);
     console.log(cal_2);
     console.log(cal_3);
-    console.log(cal_4);*/
-    const calculator = {
-        a = 3,
-        b = 2,
-        equel: function(){
-            return this.a + b;
-        }
-    }
-    console.log(calculator);
+    console.log(cal_4);
+    
    
 // 3.write any example for running the following expression fun(5)(10,3);
