31932ec769b15fdda3bb82a8082c0d055532b0d9
MaxFeldman <Mr.Feldman85@gmail.com>
2022-01-16 20:38:35 +0200

    Classwork js

--- Homework.js
+++ Homework.js
@@ -1,42 +1,16 @@
-// 1.Just used only "a" and "s" i need print "ananas"
-console.log("a" + ("as" - 2) + "as");
-// 2.function calculate that can perform any arithmetic operations on two numbers (+,-,*,/);
-function calculate (num1,num2,action){
-    switch(action){
-        case '+': return num1 + num2;break;
-        case '-': return num1 - num2;break;
-        case '*': return num1 * num2;break;
-        case '/': return num1 / num2;break;
-        default:
-            return("wrong action");
-    }
-    }
-    const cal_1 = calculate(2,3,'+');
-    const cal_2 = calculate(3,2,'-');
-    const cal_3 = calculate(3,3,'*');
-    const cal_4 = calculate(9,3,'/');
-    console.log(cal_1);
-    console.log(cal_2);
-    console.log(cal_3);
-    console.log(cal_4);
-function calculator(op1,op2,compute){
-    return compute(op1,op2);
+function encode(num, codingString){
+    
+    let res = "";
+    do{
+           
+           const digit = Math.trunc(num % codingString); 
+           const symb = getSymbol(digit, codingString);
+           res = symb + res;
+           num = Math.trunc(num/codingString);
+      }while(num >= 1);
+      return res;
 }
-function sum(op1,op2){
-    return op1 + op2;
+function getSymbol(digit,codingString){
+    return "" + digit + codingString;
 }
-    console.log('2.', calculator(a,b,sum));
-   
-// 3.write any example for running the following expression fun(5)(10,3);
-function fun(index) {
-    if(index == 5){
-        return function(a,b){
-            return a + b;
-        }
-    }else {
-        return function(a,b){
-            return a - b;
-        }
-    }
-}
-console.log('3. ', fun(5)(10,3));
+console.log(encode(61,15));
--- app.js
+++ app.js
@@ -1,15 +1,52 @@
 //if logical expression contains both string and number, JS will convert string number
 //if string doesn't contain a number a result of conversion will be NaN
 //In any logical expression if there is NaN, then the result will be false
-console.log(`"abc" > 23 is ${"abc" > 23}`);
+/*console.log(`"abc" > 23 is ${"abc" > 23}`);
 console.log(`"abc" < 23 is ${"abc" < 23}`);
 console.log(`"abc" != 23 is ${"abc" != 23}`);
 /***************************************/ 
-function stringProcessing(str){
+/*function stringProcessing(str){
     const strP = "" + str;
-    console.log(`${str}[4] is ${str[4]}`);
-    console.log(`length of ${str} is ${str.length}`);
-    console.log
+    console.log(`"${str}"[4] is ${str[4]}`);
+    console.log(`length of "${str}" is ${str.length}`);
+    console.log(`"${str}" includes "abc" is ${strP.includes("abc")}`);
+    console.log(`substring of "${str}" beginning from index 2 to index 5 is ${strP.substring(2,5)}`);
+    console.log(`index of substring "ll" in the "${str}" is ${strP.indexOf("ll")}; last index of letter l in the ${str} is ${strP.indexOf("ll")}; ${strP.lastIndexOf("ll")}`);
 }
-
-
+stringProcessing("abcd*lmn*ll(oo&&tt");
+//Home Work 11
+function encode(num, base){
+    //base from 2 to 10 
+    let res = "";
+    do{
+           const digit = Math.trunc(num % base); 
+           const symb = getSymbol(digit);
+           res = symb + res;
+           num = Math.trunc(num/base);
+      }while(num >= 1);
+      return res;
+}
+function getSymbol(digit){
+   return "" + digit;//it will work only for base <=10
+}
+console.log(encode(10,2))*/
+//HW  write function
+//
+function encode(num, codingString){
+    //getSymbol(digit, codingString)
+    //base = length of codingString
+    //algorythm the same as specifed above
+    let res = "";
+    do{
+           
+           const digit = Math.trunc(num % codingString); 
+           const symb = getSymbol(digit, codingString);
+           res = symb + res;
+           num = Math.trunc(num/codingString);
+      }while(num >= 1);
+      return res;
+}
+function getSymbol(digit,codingString){
+    return "" + digit + codingString;
+}
+console.log(encode(61,15));
